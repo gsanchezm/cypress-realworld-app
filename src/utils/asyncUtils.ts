65554754500c1d650edd66746dfa00bd7575092c
Gilberto Sanchez Mares <gilsanchezts@outlook.com>
2025-12-03 21:11:32 -0600

    Update asyncUtils.ts

--- src/utils/asyncUtils.ts
+++ src/utils/asyncUtils.ts
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-const apiUrl =
-  process.env.VITE_API_URL ||
-  `http://localhost:${process.env.VITE_BACKEND_PORT || 3001}`;
+const apiUrl = process.env.VITE_API_URL || `http://localhost:${process.env.VITE_BACKEND_PORT || 3001}`;
 
 const httpClient = axios.create({
   baseURL: apiUrl,
