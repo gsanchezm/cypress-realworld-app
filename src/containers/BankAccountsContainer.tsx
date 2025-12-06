39f7e7a888bc4ea9972f70db4834d711c6f1deed
gsanchezm <gilsanchezts@outlook.com>
2025-12-06 13:23:40 -0600

    refactor bank account container

--- src/containers/BankAccountsContainer.tsx
+++ src/containers/BankAccountsContainer.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { styled } from "@mui/material/styles";
 import { useActor } from "@xstate/react";
 import {
